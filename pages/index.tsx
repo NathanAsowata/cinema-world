81b4dcde950210666db6896cca8bf7c27feb76cc
Nathan Asowata <nathanasowata@gmail.com>
2022-08-20 18:05:39 +0100

    Set up app skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Head>
+        <title>Cinema World</title>
+        <link rel="icon" href="logo.png" />
+      </Head>
+      <h1>Cinema World</h1>
+      <a href="#">A tested link</a>
+    </>
   )
 }
 
