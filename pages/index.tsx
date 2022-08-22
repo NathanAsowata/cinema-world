c6cd65635a6ae962cddeb9d4696605c91999fcdf
Nathan Asowata <nathanasowata@gmail.com>
2022-08-22 14:05:57 +0100

    Completed trending movies section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import Head from 'next/head'
+import MovieCard from '../components/MovieCard'
 import SearchBar from '../components/SearchBar'
 import styles from '../styles/Home.module.scss'
 
-const Home = () => {
+
+interface propTypes {
+  movies: {
+    id: number,
+    poster_path: string,
+    title: string,
+    vote_average: number
+  }[]
+}
+
+const Home = ({movies}:propTypes) => {
+  
   return (
     <>
       <Head>
@@ -20,8 +32,27 @@ const Home = () => {
         <h1>Explore millions of movies and TV shows</h1>
         <SearchBar />
       </header>
+      <section className={styles.trendingSection}>
+        {movies ? movies.map(movie => {
+          return(
+            <MovieCard movie={movie} key={movie.id} />
+          )
+        }): <h1>Loading</h1>}
+      </section>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+ 
+  const res = await fetch(`https://api.themoviedb.org/3//trending/movie/week?api_key=${process.env.REACT_APP_KEY}`)
+  const data = await res.json()
+  
+  return  {
+    props: {
+      movies: data.results
+    }
+  }
 }
 
 export default Home
