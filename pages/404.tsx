f889e4a766719ebe32d8acf863baed765e7259e3
Nathan Asowata <nathanasowata@gmail.com>
2022-08-22 18:13:46 +0100

    Updated 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 import styles from '../styles/Error.module.scss'
 
 
 const Error = () => {
     return(
-        <h1 className={styles.title}>404 | This page could not be found.</h1>
+        <>
+        <Head>
+            <title>404 | Page not found</title>
+            <link rel="icon" href="logo.png" />
+        </Head>
+            <h1 className={styles.title}>404 | This page could not be found.</h1>
+        </>
     )
 }
 
