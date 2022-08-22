5c101bbe0501075975018c84e9d7dc26bc732e0e
Nathan Asowata <nathanasowata@gmail.com>
2022-08-22 18:09:12 +0100

    Created 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Error.module.scss'
+
+
+const Error = () => {
+    return(
+        <h1 className={styles.title}>404 | This page could not be found.</h1>
+    )
+}
+
+export default Error
